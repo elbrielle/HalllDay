5456ea35245410eb315039788633ca6b5c73e29e
notsointeresting <eli311.lucero@gmail.com>
2026-01-15 11:36:05 -0600

    feat: Update Flutter service worker version and bootstrap file hash.

--- static/flutter_service_worker.js
+++ static/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3d960f3b058f0aeb8e11dbdab03f1b62",
+const RESOURCES = {"flutter_bootstrap.js": "b175164bb458003c01b2a28eca34740b",
 "version.json": "2b521e10dfa0f067561de489a19d6620",
 "index.html": "924e12d6bcb7e0caa0a59ed2142a82e9",
 "/": "924e12d6bcb7e0caa0a59ed2142a82e9",
